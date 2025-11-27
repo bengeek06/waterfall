180ab784277761b62a8ccfa6ba758483fca3f84a
bengeek <bengeek06@gmail.com>
2025-11-27 07:23:19 +0100

    feat: add diagram_service as submodule and integrate into compose files
    - Added diagram-api-waterfall as git submodule in services/diagram_service
    - Integrated diagram_service into all docker-compose files:
      * docker-compose.backend.yml
      * docker-compose.test.yml
      * docker-compose.yml
    - Configuration:
      * Port 5007:5000
      * Database: diagram_staging
      * Guardian integration enabled
      * All standard environment variables
    - Added diagram_staging database to init-db.sh script
    - Added Diagram API to swagger-ui configuration
    - Added DIAGRAM_SERVICE_URL to web_service environment variables

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -10,6 +10,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE basic_io_staging;
     CREATE DATABASE storage_staging;
     CREATE DATABASE project_staging;
+    CREATE DATABASE diagram_staging;
     
     GRANT ALL PRIVILEGES ON DATABASE auth_staging TO $POSTGRES_USER;
     GRANT ALL PRIVILEGES ON DATABASE identity_staging TO $POSTGRES_USER;
@@ -17,6 +18,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE basic_io_staging TO $POSTGRES_USER;
     GRANT ALL PRIVILEGES ON DATABASE storage_staging TO $POSTGRES_USER;
     GRANT ALL PRIVILEGES ON DATABASE project_staging TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE diagram_staging TO $POSTGRES_USER;
 EOSQL
 
 echo "Database initialization completed successfully"
